7473d0287bd37238fd3b7544f8c97471837ae241
mibelcal <mibelcal@yahoo.es>
2024-01-22 12:37:03 +0100

    2a US LIsta de clientes OK

--- MyPersonalBankAPI_Maven/src/test/java/es/netmind/mypersonalbankapi/persistencia/ClientesDBRepoJPATest.java
+++ MyPersonalBankAPI_Maven/src/test/java/es/netmind/mypersonalbankapi/persistencia/ClientesDBRepoJPATest.java
@@ -2,6 +2,7 @@ package es.netmind.mypersonalbankapi.persistencia;
 
 import com.mysql.cj.xdevapi.Client;
 import es.netmind.mypersonalbankapi.config.SpringConfig;
+import es.netmind.mypersonalbankapi.modelos.clientes.Cliente;
 import es.netmind.mypersonalbankapi.modelos.clientes.Empresa;
 import es.netmind.mypersonalbankapi.modelos.clientes.Personal;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +18,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
@@ -40,9 +44,14 @@ class ClientesDBRepoJPATest {
         em = emf.createEntityManager();
     }
 
-
     @Test
-    void getAll() {
+    void getAll() throws Exception {
+        List<Cliente> clientes = repo.getAll();
+        System.out.println("Clientes:"+clientes.toString());
+        System.out.println("Num Clientes:"+clientes.size());
+
+        assertNotNull(clientes);
+        assertThat(clientes.size(), greaterThan(0));
     }
 
     @Test
