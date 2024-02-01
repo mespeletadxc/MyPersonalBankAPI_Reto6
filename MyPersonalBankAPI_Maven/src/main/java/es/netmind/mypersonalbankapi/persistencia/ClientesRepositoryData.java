ae5da52f2774fffc03737424fb43f1e61a48041d
unknown <maria.espeleta@dxc.com>
2024-02-01 13:25:05 +0100

    transaccion propagacion en repoData

--- MyPersonalBankAPI_Maven/src/main/java/es/netmind/mypersonalbankapi/persistencia/ClientesRepositoryData.java
+++ MyPersonalBankAPI_Maven/src/main/java/es/netmind/mypersonalbankapi/persistencia/ClientesRepositoryData.java
@@ -2,6 +2,10 @@ package es.netmind.mypersonalbankapi.persistencia;
 
 import es.netmind.mypersonalbankapi.modelos.clientes.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(propagation = Propagation.MANDATORY)
 
 public interface ClientesRepositoryData extends JpaRepository<Cliente, Integer> {
 }
